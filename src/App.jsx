3004d035b880ec1d213b7ad3b1f1e3ab4aec72a3
Daniel Victor <danielvictoraires9@gmail.com>
2025-10-15 22:04:12 -0300

    📱 Add src/App.jsx - Componente principal da aplicação

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomePage from '../be.link.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  )
+}
+
+export default App
